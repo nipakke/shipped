75878007d22359ea0238d01768b070eba3abf6e0
nipakke <66799869+nipakke@users.noreply.github.com>
2026-03-26 03:50:48 +0100

    fix: slugify returns empty string (#29)

--- libs/strings/slugify.ts
+++ libs/strings/slugify.ts
@@ -1,8 +1,39 @@
 import { deburr } from "es-toolkit/string";
 
-export function slugify(s: string): string {
-  return deburr(s)
+function getShortHash(str: string) {
+  let hash = 5381;
+  for (let i = 0; i < str.length; i++) {
+    // Using "| 0" or ">>> 0" keeps it within 32-bit integer range
+    hash = ((hash << 5) + hash + str.charCodeAt(i)) | 0;
+  }
+  return Math.abs(hash).toString(36);
+}
+
+export function slugify(
+  s: string,
+  opts?: {
+    /**
+     * Always add the hash at the end of the slug to avoid collisions
+     *
+     * @default false
+     */
+    preventCollisions?: boolean;
+  },
+): string {
+  const baseSlug = deburr(s)
     .toLowerCase()
     .replace(/[^a-z0-9]+/g, "-")
     .replace(/^-+|-+$/g, "");
+
+  const hash = getShortHash(s);
+
+  // If the string was ONLY special characters, baseSlug is empty.
+  // In that case, just return the hash.
+
+  if (opts?.preventCollisions) {
+    return baseSlug ? `${baseSlug}-${hash}` : hash;
+  }
+
+  if (!baseSlug.length) return hash;
+  return baseSlug;
 }
