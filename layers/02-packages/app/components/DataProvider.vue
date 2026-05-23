cdc00949ad8190be367f82a8e9dc56025faeafad
nipakke <nipakke@gmail.com>
2026-05-23 09:40:19 +0200

    refactor(DataProvider): clean up unused code and simplify data fetching

--- layers/02-packages/app/components/DataProvider.vue
+++ layers/02-packages/app/components/DataProvider.vue
@@ -1,8 +1,7 @@
 <template>
-  <slot :data="data" :error="query.error" :isLoading="query.pending" :isRetrying="false" :refetch="refetch"
-    :canRetry="canRetryFlag" v-bind="{
-      isPending: query.pending,
-    }" />
+  <slot :data="data" :error="query.error" :isLoading="query.pending" :isRetrying="false" v-bind="{
+    isPending: query.pending,
+  }" />
 </template>
 
 <script setup lang="ts">
@@ -17,57 +16,14 @@ const props = defineProps<{
 
 const rpc = useRPC();
 
-const isPrerendering = import.meta.prerender;
-
-console.log("VALID:", props.config.packageId)
-
-const query = await useAsyncData(`package-${props.config.packageId}`, () => rpc.packages.getOne({
-  // experimental_prefetchInRender: true,
-  // refetchInterval: isPrerendering ? false : 60_000,
-  // refetchOnWindowFocus: isPrerendering ? false : true,
-  packageId: props.config.packageId,
-  // enabled: isPrerendering ? true : import.meta.client,
-}), {
-  server: true,
-  // server: import.meta.prerender,
-})
-
-
-/* const query = useQuery(
-  rpc.packages.getOne.queryOptions({
-    retry: 2,
-    experimental_prefetchInRender: true,
-    refetchInterval: isPrerendering ? false : 60_000,
-    refetchOnWindowFocus: isPrerendering ? false : true,
-    input: {
+const { data, ...query } = await useAsyncData(
+  `package-${props.config.packageId}`,
+  () =>
+    rpc.packages.getOne({
       packageId: props.config.packageId,
-    },
-    enabled: isPrerendering ? true : import.meta.client,
-  }),
-);
-
-if (isPrerendering) {
-  await query.suspense();
-} */
-
-watch(
-  () => props.config,
-  (val, oldVal) => {
-    if (val.packageId != oldVal.packageId) {
-      // query.refetch();
-    }
+    }),
+  {
+    server: import.meta.prerender,
   },
 );
-
-const data = computed<Package | undefined>(() => {
-  return query.data.value ?? undefined; /*  ? PackageData.make(query.data.value) : undefined; */
-});
-
-const canRetryFlag = computed(() => false);
-
-function refetch() {
-  if (canRetryFlag.value) {
-    // query.refetch();
-  }
-}
 </script>
